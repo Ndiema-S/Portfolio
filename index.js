99e4aa84efdb68c7f06c2d4361bee16c0e3710c8
Ndiema-S <you@example.com>
2022-09-10 20:41:05 +0300

    add landing page and description section

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navlinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const cards = document.querySelectorAll('.cards');
 
 hamburger.addEventListener("click", () => {
     navlinks.classList.toggle("open");
 });
+
+cards.addEventListener("hoveer", ())
